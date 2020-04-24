b1eef37f60bf597dd78a4e32f8a333ac42764102
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2020-04-24 22:36:42 +0200

    login/out

--- users/views.py
+++ users/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterFrom
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterFrom(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for You are albe to login...{username}!')
+            return redirect('login')
+        else:
+            messages.error(request, f'Somethong went wrong !')
     else:
-        form = UserCreationForm()
+        form = UserRegisterFrom()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
